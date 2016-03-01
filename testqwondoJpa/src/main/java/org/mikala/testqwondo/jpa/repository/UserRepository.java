ac283b3a365b8cf94d0910f8b6149351535b3fcc
kalanskim <kalanskim@zetokatowice.pl>
2016-03-01 07:49:22 +0100

    refactor

--- testqwondoJpa/src/main/java/org/mikala/testqwondo/jpa/repository/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package org.mikala.testqwondo.jpa.repository;
-import org.mikala.testqwondo.api.model.User;
-import org.springframework.data.repository.CrudRepository;
-public interface UserRepository extends CrudRepository<User, Long>{
-
-}
